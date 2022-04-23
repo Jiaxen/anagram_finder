d2d6e177e677d9848b3db8b56253beeea452e74b
jiaxen <jiaxen.lau@gmail.com>
2022-04-23 22:55:46 +0100

    Reformatted

--- anagram_generator.py
+++ anagram_generator.py
@@ -28,15 +28,19 @@ class AnagramGenerator:
         input_word = self.clean_word(input_word)
         letter_count = Counter(input_word)
         word_len = len(input_word)
-        for anagram in self.generate_anagram_helper(letter_count, [], self.root_node, word_len):
+        for anagram in self.generate_anagram_helper(
+            letter_count, [], self.root_node, word_len
+        ):
             print(anagram)
 
     def generate_anagram_helper(self, letter_count, builder, node, target_length):
         if node.leaf:
-            if len([x for x in builder if x != ' ']) >= target_length:
+            if len([x for x in builder if x != " "]) >= target_length:
                 yield "".join(builder)
             builder.append(" ")
-            for res in self.generate_anagram_helper(letter_count, builder, self.root_node, target_length):
+            for res in self.generate_anagram_helper(
+                letter_count, builder, self.root_node, target_length
+            ):
                 yield res
             builder.pop()
         for letter, sub_node in node.children.items():
@@ -44,7 +48,9 @@ class AnagramGenerator:
                 continue
             letter_count[letter] -= 1
             builder.append(letter)
-            for res in self.generate_anagram_helper(letter_count, builder, sub_node, target_length):
+            for res in self.generate_anagram_helper(
+                letter_count, builder, sub_node, target_length
+            ):
                 yield res
             builder.pop()
             letter_count[letter] += 1
--- main.py
+++ main.py
@@ -5,7 +5,8 @@ def main():
     anagram_generator = AnagramGenerator()
     anagram_generator.min_word_length = 4
     anagram_generator.load_word_list("word_list/words_alpha.txt")
-    anagram_generator.generate_anagram('onmayd')
+    anagram_generator.generate_anagram("onmayd")
+
 
 if __name__ == "__main__":
     main()
