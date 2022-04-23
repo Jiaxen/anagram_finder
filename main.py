a1f98f60e6a62811394bafa3001f7641e00aeac0
jiaxen <jiaxen.lau@gmail.com>
2022-04-23 23:23:53 +0100

    Input interface

--- main.py
+++ main.py
@@ -5,7 +5,11 @@ def main():
     anagram_generator = AnagramGenerator()
     anagram_generator.min_word_length = 4
     anagram_generator.load_word_list("word_list/words_alpha.txt")
-    anagram_generator.generate_anagram("onmayd")
+    while True:
+        input_word = input('Find the anagram of: ')
+        if not input_word:
+            break
+        anagram_generator.generate_anagram(input_word)
 
 
 if __name__ == "__main__":
