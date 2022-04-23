f2de9e5698b729744eac5646e2bcfcfd5f09c3af
jiaxen <jiaxen.lau@gmail.com>
2022-04-23 21:51:20 +0100

    Refactor node class from anagram generator

--- anagram_generator.py
+++ anagram_generator.py
@@ -1,27 +1,30 @@
 import re
 
 
-class AnagramNode:
-    def __init__(self, letter='', depth=0, leaf=False):
-        self.letter = letter
-        self.depth = depth
-        self.leaf = leaf
-        self.children = {}
-
-    def load_word(self, word):
-        node = self
-        for index, letter in enumerate(word):
-            if letter not in node.children:
-                node.children[letter] = AnagramNode(letter=letter, depth=node.depth+1, leaf=(index == len(word)-1))
-            node = node.children[letter]
+class AnagramGenerator:
+    def __init__(self):
+        self.root_node = AnagramNode()
 
     def load_word_list(self, path):
         with open(path, 'r') as f:
             for word in f.readlines():
                 word = word.lower()
                 word = re.sub(r"[^a-z]", "", word)
-                self.load_word(word)
+                self.load_letters(word)
 
-    def generate_anagrams(self):
+    def load_letters(self, word):
+        node = self.root_node
+        for index, letter in enumerate(word):
+            if letter not in node.children:
+                node.children[letter] = AnagramNode(letter=letter, depth=node.depth+1, leaf=(index == len(word)-1))
+            node = node.children[letter]
+
+    def generate_anagram(self, input_word):
         pass
 
+class AnagramNode:
+    def __init__(self, letter='', depth=0, leaf=False):
+        self.letter = letter
+        self.depth = depth
+        self.leaf = leaf
+        self.children = {}
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from anagram_generator import AnagramNode
+from anagram_generator import AnagramGenerator
 
 def main():
-    anagram_generator = AnagramNode()
+    anagram_generator = AnagramGenerator()
     anagram_generator.load_word_list('word_list/words_alpha.txt')
-    print(anagram_generator.children)
+    print(anagram_generator.root_node.children)
 
 if __name__ == '__main__':
     main()
