a792916f4fead0fb2300aaf7624bf48b74e02893
Jiaxen <jiaxen.lau@gmail.com>
2022-04-27 17:43:14 +0100

    Added a simpler word list

--- README.md
+++ README.md
@@ -5,4 +5,5 @@ Open up main.py and hit run in your IDE!
 
 (or run ```python main.py``` in a terminal)
 
-Words are sourced from https://github.com/dwyl/english-words
+words_alpha.txt has more (and crazier) words. Source: https://github.com/dwyl/english-words
+words_simple.txt has fewer (and more normal) words. Source: http://www.gwicks.net/dictionaries.htm
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from anagram_generator import AnagramGenerator
 def main():
     anagram_generator = AnagramGenerator()
     anagram_generator.min_word_length = 4
-    anagram_generator.load_word_list("word_list/words_alpha.txt")
+    anagram_generator.load_word_list("word_list/words_simple.txt")
     while True:
         input_word = input('Find the anagram of: ')
         if not input_word:
