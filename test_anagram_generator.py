ad22e189386d3913a141d5a08e8d56361b946ed0
Jiaxen <jiaxen.lau@gmail.com>
2022-04-27 17:55:17 +0100

    Fixed test condition

--- test_anagram_generator.py
+++ test_anagram_generator.py
@@ -13,6 +13,7 @@ def test_clean_word(new_anagram_generator):
 
 def test_anagram_generator(new_anagram_generator, capsys):
     input_word = 'onmayd'
+    new_anagram_generator.min_word_length = 5
     new_anagram_generator.load_word_list("word_list/words_alpha.txt")
     new_anagram_generator.generate_anagram(input_word)
     captured = capsys.readouterr()
